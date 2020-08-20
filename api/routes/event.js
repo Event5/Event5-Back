23b42a51f7f2919e5721d532f34101760254121a
Luis Chavez <josephsiul15@gmail.com>
2020-08-20 01:34:10 -0500

    Finish of signup and signin

---
+++ api/routes/event.js
@@ -0,0 +1,9 @@
+// const express = require('express');
+
+// const config = require('../config/config');
+
+// function eventApi(app) {
+//   const router = express.Router();
+//   app.use('/api/event')
+
+// }
--- api/utils/mocks/usersMocks.js
+++ api/utils/mocks/usersMocks.js
@@ -1,24 +1,10 @@
 const usersMock = [
   {
     id: 1,
-    username: 'Jose',
-    email: 'jose@gmail.com',
-    password: 'jose123',
-    type: 'admin',
-  },
-  {
-    id: 1,
     username: 'Luis',
     email: 'luis@gmail.com',
     password: '$2b$05$q9Fu3TgU6locFXjoAPrtt.ueAvfDpi0pHjxi12DKOgk0bf7SrOHMi',
     type: 'admin',
-  },
-  {
-    id: 3,
-    username: 'Pancho',
-    email: 'pancho@gmail.com',
-    password: 'pancho123',
-    type: 'organizer',
   },
 ];
 
@@ -39,8 +25,6 @@ function createUser(user) {
   user.id = usersMock.length + 1;
   return usersMock.push(user);
 }
-
-// console.log(`getUser__ ${getUser('luis@gmail.com')}`);
 
 module.exports = {
   getUser,
